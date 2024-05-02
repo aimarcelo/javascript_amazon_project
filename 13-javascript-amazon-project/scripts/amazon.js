37c917408546c2a197e766bc43e8fba568329ea5
Alyssa Marcelo <alyssamarcelo17@gmail.com>
2024-05-03 00:52:23 +0800

    Fix quantity bug on Add To Cart button

--- 13-javascript-amazon-project/scripts/amazon.js
+++ 13-javascript-amazon-project/scripts/amazon.js
@@ -30,7 +30,7 @@ products.forEach((product) => {
           </div>
 
           <div class="product-quantity-container">
-            <select>
+            <select class="js-quantity-selector-${product.id}">
               <option selected value="1">1</option>
               <option value="2">2</option>
               <option value="3">3</option>
@@ -73,7 +73,9 @@ document.querySelectorAll('.js-add-to-cart')
   .forEach((button) => {
     button.addEventListener('click', () => {
       const productId = button.dataset.productId;
-      addToCart(productId);
+      const quantitySelectorElement = document.querySelector(`.js-quantity-selector-${productId}`);
+      const quantityToAdd = Number(quantitySelectorElement.value);
+      addToCart(productId, quantityToAdd);
       updateCartQuantity();
     });
   });
